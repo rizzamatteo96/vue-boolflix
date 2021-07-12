9d848963d6c4b32d35ec0a379db6d9735ce537a0
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-12 23:09:25 +0200

    add flags on language

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,15 +2,35 @@
   <div class="card">
     Titolo = {{film.title}} <br>
     Titolo originale = {{film.original_title}} <br>
-    Lingua = {{film.original_language}} <br>
+    Lingua = <img :src="getImgUrl(language)" v-bind:alt="language"> <br>
     Voto = {{film.vote_average}} <br>
   </div>
+
 </template>
 
 <script>
 export default {
   name : 'Card',
-  props : ['film']
+  props : ['film'],
+  data(){
+    return{
+      language : this.film.original_language
+    }
+  },
+  methods : {
+    getImgUrl(pic) {
+      try {
+        let fileName = require('../assets/img/' + pic + '.png');
+        // console.log("file found");
+        // console.log(fileName);
+        return fileName
+      } 
+      catch (e) {
+        // console.log("sorry, file not found" + e);
+        return 'nothing'
+      }  
+    }
+  }
 }
 </script>
 
@@ -21,5 +41,9 @@ export default {
     border-radius: 20px;
     border: 1px solid red;
     padding: 20px;
+
+    img{
+      height: 1rem;
+    }
   }
 </style>
